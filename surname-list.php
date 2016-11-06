cb957fc149f726f981c1ab1e3f0430a081853199
William Bell <william.bell@frog.za.net>
2016-11-06 19:40:33 +0200

    lint surname-list.php

--- surname-list.php
+++ surname-list.php
@@ -1,65 +1,64 @@
 <?php
-/*
+/**
  Grampsphpexporter.- export gramps genealogy program to the web
 
- Copyright (C) 2012  William Bell <william.bell@frog.za.net>
+ Copyright (C) 2012-2016 William Bell <william.bell@frog.za.net>
 
-    Grampsphpexporter is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+        Grampsphpexporter is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
 
-    Grampsphpexporter is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+        Grampsphpexporter is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+        GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+        You should have received a copy of the GNU General Public License
+        along with Foobar.    If not, see <http://www.gnu.org/licenses/>.
 */
-  require_once 'template.php';
-  echo head('Surnames', '');
-  print("\n<h3>Surnames</h3>\n");
-  try
-  {
+require_once 'template.php';
+echo head('Surnames', '');
+print("\n<h3>Surnames</h3>\n");
+try
+{
     //open the database
     $db = new PDO('sqlite:../../.sqlite/gramps1.db');
 
     //now output the data to a simple html table...
 
-    $result = $db->query('
-		select
-			surname,
-			count(1) as Number
-		from surname
-		group by surname
-		order by upper(surname)');
- 	$prevLetter = 'ZZZ';
-    foreach($result as $row)
-    {
-		$surname = $row['surname'];
-		if ($surname == '')
-		{
-			$surname = "Unknown";
-		}
-		$currentLetter = strtoupper($surname[0]);
-		if ($prevLetter != $currentLetter)
-		{
-			if ($prevLetter != 'ZZZ')
-				print("</div>\n");
-			$prevLetter = $currentLetter;
-			print("<div class=\"section\">\n\t<div class=\"letter\">".$prevLetter."</div>\n");
-		}
-		print("\t<div class=\"surname\"><a href=\"surname.php?surname=".$surname."\">".$surname."</a> (".$row['Number'].")</div>\n");
+    $result = $db->query(
+        "select
+            surname,
+            count(1) as Number
+        from surname
+        group by surname
+        order by upper(surname)"
+    );
+    $prevLetter = 'ZZZ';
+    foreach ($result as $row) {
+        $surname = $row['surname'];
+        if ($surname == '') {
+            $surname = "Unknown";
+        }
+        $currentLetter = strtoupper($surname[0]);
+        if ($prevLetter != $currentLetter) {
+            if ($prevLetter != 'ZZZ') {
+                print("</div>\n");
+            }
+            $prevLetter = $currentLetter;
+            print("<div class=\"section\">\n\t<div class=\"letter\">".$prevLetter."</div>\n");
+        }
+        print("\t<div class=\"surname\"><a href=\"surname.php?surname=".$surname."\">".$surname."</a> (".$row['Number'].")</div>\n");
     }
-	print("</div>\n");
+    print("</div>\n");
 
     // close the database connection
-    $db = NULL;
-  }
-  catch(PDOException $e)
-  {
+    $db = null;
+}
+catch(PDOException $e)
+{
     print 'Exception : '.$e->getMessage();
-  }
-  echo foot();
+}
+echo foot();
 ?>
