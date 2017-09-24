f41c71d965a7c80b5dfaf9f76cf1c720e03ebea5
William Bell <william.bell@frog.za.net>
2017-09-24 18:26:32 +0200

    change to HTML5

--- template.php
+++ template.php
@@ -19,11 +19,10 @@
 */
 function head($title, $surname)
 {
-   $html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
-   \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+   $html = "<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />
+<meta charset=\"UTF-8\" />
 <link rel=\"stylesheet\" type=\"text/css\" href=\"basic.css\" />
 
 
